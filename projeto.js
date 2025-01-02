e58a023a326db0b7f91dce89a87cd4046faf5074
carlos alberto <carlosalbertosnb20@gmail.com>
2025-01-01 22:20:34 -0300

    atualizado

---
+++ projeto.js
@@ -0,0 +1,32 @@
+
+
+
+function simbolo(){
+    const array = ['+','/','-']
+    const len = array.length
+    const num = Math.floor(Math.random()*len)
+    const valor = array[num]
+
+    console.log(valor)
+
+}
+
+simbolo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
