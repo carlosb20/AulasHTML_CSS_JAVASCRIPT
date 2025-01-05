c4e353ffdc202c7b4cf973b9016eb619cdc54cfc
carlos alberto <carlosalbertosnb20@gmail.com>
2025-01-04 23:46:23 -0300

    a

---
+++ aula01.js
@@ -0,0 +1,33 @@
+
+
+const cont = [...document.querySelectorAll(".cont")]
+const move = [...document.querySelectorAll("#move")]
+
+
+
+cont.map((el)=>{
+    el.addEventListener("click",(item)=>{
+
+        const ca = item.target
+        ca.classList.add("getclass")
+       
+        
+        })
+    })
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
