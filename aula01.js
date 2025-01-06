d465372250f06f85910e8f9a30c2979721cd1b8c
carlos alberto <carlosalbertosnb20@gmail.com>
2025-01-05 23:35:39 -0300

    atu

--- aula01.js
+++ aula01.js
@@ -3,6 +3,9 @@
 const cont = [...document.querySelectorAll(".cont")]
 const move = [...document.querySelectorAll("#move")]
 
+const val = document.querySelector(".conteiner")
+
+
 
 
 cont.map((el)=>{
@@ -10,11 +13,25 @@ cont.map((el)=>{
 
         const ca = item.target
         ca.classList.add("getclass")
-       
         
+             
         })
     })
     
+
+const div3 = document.createElement('div')
+div3.setAttribute("id","ca")
+div3.setAttribute("class","cont")
+
+div3.innerHTML = 'createElement'
+val.appendChild(div3)
+
+
+console.log(div3)
+
+
+
+
 
 
 
