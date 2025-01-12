3e330915ff517106815f1c5790cbed46f96f23f7
carlos alberto <carlosalbertosnb20@gmail.com>
2025-01-12 00:30:22 -0300

    atualizado

--- aula02.js
+++ aula02.js
@@ -1,1 +1,56 @@
 
+const val = [...document.querySelectorAll(".cont")]
+const sal = [...document.querySelectorAll(".cont1")]
+
+
+
+const move=()=>{
+    const lev = [...document.querySelectorAll(".cont")]
+    lev.map((sa)=>{
+        sa.classList.remove("getcont")
+    })
+}
+
+
+
+const pega=(s)=>{
+    
+    ca = s
+    
+}
+
+var ca = ''
+
+sal.map((da)=>{
+    da.addEventListener("click",(de)=>{
+        //move()
+        //de.classList.add("getcont")
+        de.target.innerHTML = ca
+        
+       
+    })
+})
+
+
+
+val.map((le)=>{
+    le.addEventListener("click",(el)=>{
+       move()
+       le.classList.add("getcont")   
+        pega(el.target.textContent)
+    })
+     
+})
+    
+
+
+
+
+
+
+
+
+
+
+
+
