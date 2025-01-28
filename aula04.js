97d974b673a94169e1c3e1abd20e11e7dffc1b61
carlos alberto <carlosalbertosnb20@gmail.com>
2025-01-28 20:48:55 -0300

    atu

--- aula04.js
+++ aula04.js
@@ -7,7 +7,7 @@ async function loadfilmes() {
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}`
     const resposta = await fetch(url) 
     const resp = await resposta.json()
-    
+    ret
     console.log(resp)
     
 
