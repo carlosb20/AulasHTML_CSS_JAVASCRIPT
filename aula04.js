7b868595fd2dbd3469ffb4fd0e2c903d255c871c
carlos alberto <carlosalbertosnb20@gmail.com>
2025-01-27 21:03:00 -0300

    a

--- aula04.js
+++ aula04.js
@@ -1,17 +1,29 @@
 
 
-
- 
-
 const  key = '6d9a1e25dbcb17a0e630bab33da79bce'
 
 
-async function Filmes02(mgs) {
-
-    const url = mgs
+async function loadfilmes() {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}`
     const resposta = await fetch(url) 
     const resp = await resposta.json()
+    
     console.log(resp)
+    
+
+
 }
 
-Filmes02(`https://api.themoviedb.org/3/discover/movie?api_key=${key}`)
+loadfilmes()
+
+
+const btn = document.querySelector("#sa") 
+
+
+btn.addEventListener("click",(el)=>{
+
+    console.log('ok',el)
+
+
+})
+
