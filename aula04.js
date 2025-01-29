a506d127e997ca81306922c2666973ab051b209e
carlos alberto <carlosalbertosnb20@gmail.com>
2025-01-28 23:55:08 -0300

    atia

--- aula04.js
+++ aula04.js
@@ -3,17 +3,32 @@
 const  key = '6d9a1e25dbcb17a0e630bab33da79bce'
 
 
-async function loadfilmes() {
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}`
+async function loadfilmes(ms) {
+    const url = ms
     const resposta = await fetch(url) 
     const resp = await resposta.json()
-    ret
-    console.log(resp)
     
-
+    return resp.results
 
 }
 
-loadfilmes()
+const filmes = loadfilmes(`https://api.themoviedb.org/3/discover/movie?api_key=${key}`)
 
+const caixa = document.querySelector(".caixa")
 
+filmes.then(function(va){
+    va.map((el)=>{
+
+        console.log(el)
+        const divimg = document.createElement('div')
+        divimg.setAttribute("class","Img")
+        divimg.innerHTML = `
+                        <img src= https://image.tmdb.org/t/p/original${el.poster_path} width=150px height=150px/>
+                        <p> ${el.title}</p>
+
+        `
+
+        caixa.appendChild(divimg)
+    })
+    
+})
