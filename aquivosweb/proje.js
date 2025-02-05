2db52ba012e2a1b79a3127f2480812c27f9cc17b
carlos alberto <carlosalbertosnb20@gmail.com>
2025-02-04 22:40:36 -0300

    atualizado

--- aquivosweb/proje.js
+++ aquivosweb/proje.js
@@ -2,22 +2,32 @@
 
 
 class Matematica{
-
+    
     simbolo() {
         let array = ['+','-','x']
         let len = array.length
-        let num = Math.floor(Math.random(),len)
-
-        console.log(num)
+        let num = Math.floor(Math.random()*len)
+        let simbo = array[num]
+        return simbo
         
+    }
+    numero(){
+
+        let val = Math.floor(Math.random()*10)
+        return val
+    }
+    soma(){
+
+        console.log(this.simbolo())
+        console.log(this.numero())
     }
 }
 
 
-const sim = Matematica()
+const sim = new Matematica()
 
+sim.soma()
 
-sim.simbolo()
 
 
 
